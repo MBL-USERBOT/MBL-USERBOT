23bc648f74d842bfb0c74a2abd9a062225301429
MBL-USERBOT <76051599+MBL-USERBOT@users.noreply.github.com>
2020-12-15 19:40:10 +0530

    Credit

---
+++ userbot/New.py
@@ -0,0 +1,12 @@
+""" App running in this context is a very good userbot for your Telegram account inspired by 
+                 FRIDAY USERBOT 
+                 DARK COBRA USERBOT
+                 PHANTOM USETBOT
+                 HELL USERBOT 
+                 MARSHMELLOW USERBOT
+            And a great thanks to uniborg    """   
+
+
+#Support uniborg 
+                 #MBL-GANG 
+        
