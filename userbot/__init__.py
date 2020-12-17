0642a90887e2140a8dabcf76da960a0f7e0eb98b
MBL-USERBOT <76051599+MBL-USERBOT@users.noreply.github.com>
2020-12-17 12:10:46 +0530

    Sed life

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -133,7 +133,7 @@ if bool(ENV):
     # Upstream Repo
     UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/Marshmellow098/MBL-USERBOT.git")
+    "https://github.com/MBL-USERBOT/MBL-USERBOT.git")
 
     # Last.fm Module
     BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
