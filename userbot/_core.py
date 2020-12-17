9a3cb3bc667b6367847bb45324315b395baaa294
Marshmellow098 <74300487+Marshmellow098@users.noreply.github.com>
2020-12-17 19:26:29 +0530

    Hogaya

--- userbot/_core.py
+++ userbot/_core.py
@@ -11,7 +11,7 @@ DELETE_TIMEOUT = 5
 
 
 
-thumb_image_path = "./Resources/PicsArt_12-07-03.35.32.jpg"
+thumb_image_path = "./Resources/IMG-20201216-WA0001.jpg"
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "MBL"
 
